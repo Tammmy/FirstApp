869e3078c0051f4f190121806016f95d8760b5b7
tangxueda <tangxueda@hisense.com>
2016-08-13 17:39:30 +0800

    0813

---
+++ app/src/main/java/com/tammy/homework/homwwork/CollectionActivity.java
@@ -0,0 +1,99 @@
+package com.tammy.homework.homwwork;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Homework app
+ * Created by xueda on 2016/08/12.
+ */
+public class CollectionActivity extends FragmentActivity {
+
+    CollectionPagerAdapter mCollectionPagerAdapter;
+
+    /**
+     * The {@link android.support.v4.view.ViewPager} that will display the object collection.
+     */
+    ViewPager mViewPager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+
+        // Create an adapter that when requested, will return a fragment representing an object in
+        // the collection.
+        //
+        // ViewPager and its adapters use support library fragments, so we must use
+        // getSupportFragmentManager.
+        mCollectionPagerAdapter = new CollectionPagerAdapter(getSupportFragmentManager());
+
+        // Set up action bar.
+        final ActionBar actionBar = getActionBar();
+
+        // Specify that the Home button should show an "Up" caret, indicating that touching the
+        // button will take the user one step up in the application's hierarchy.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the ViewPager, attaching the adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCollectionPagerAdapter);
+    }
+
+    /**
+     * A {@link android.support.v4.app.FragmentStatePagerAdapter} that returns a fragment
+     * representing an object in the collection.
+     */
+    public static class CollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        public CollectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new ObjectFragment();
+            Bundle args = new Bundle();
+            args.putInt(ObjectFragment.ARG_OBJECT, i + 1); // Our object is just an integer :-P
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // For this contrived example, we have a 100-object collection.
+            return 10;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "OBJECT " + (position + 1);
+        }
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply displays dummy text.
+     */
+    public static class ObjectFragment extends Fragment {
+
+        public static final String ARG_OBJECT = "object";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_collection_object, container, false);
+            Bundle args = getArguments();
+            ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                    Integer.toString(args.getInt(ARG_OBJECT)));
+            return rootView;
+        }
+    }
+}
